2e83e341aa45beafff3cba82431fb0492658fa16
WhisperH <395547903@qq.com>
2023-11-04 11:57:15 +0800

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -20,8 +20,9 @@ class Track2D(QWidget):
         self.video_name_without_extension = None
         self.initUI()
         self.writer = nsq.Writer(['127.0.0.1:4150'])
-        # self.write_addr = 'http://127.0.0.1:4150/pub?topic=test'
-        # self._mutex = QMutex()
+
+
+
 
     def initUI(self):
         self.setWindowTitle('2DTrack')
@@ -66,8 +67,18 @@ class Track2D(QWidget):
 
         self.setLayout(layout)
 
+    def send_message(self, topic, transfer_data):
+        # 当writer连接nsqd成功后, 再推送数据
+        if not self.writer.conns:
+            return self.io_loop.add_callback(
+                partial(self.send_message, *(topic, transfer_data))
+            )
+
+        # 推送数据
+        self.writer.pub(topic, transfer_data, self.finish_pub)
+
     def recv_message(self, message):
-        message.enable_async()
+        # message.enable_async()
         recv_data = json.loads(message.body)
         return recv_data
 
@@ -95,11 +106,15 @@ class Track2D(QWidget):
         }
         transfer_data = json.dumps(data).encode('utf8')
         # http://www.noobyard.com/article/p-ajlbccog-kq.html
-        self.writer.pub(
-            'Upload_Vid_Channel',
-            transfer_data,
-            self.finish_pub
+        # 将producer函数加入到io_loop的callbacks列表中
+        # 实例化IOLoop
+        self.io_loop = tornado.ioloop.IOLoop.instance()
+        self.io_loop.add_callback(
+            partial(self.send_message, *('Upload_Vid_Channel', transfer_data))
         )
+        # 启动IOLoop, 它会在内部执行callbacks列表中的函数
+        self.io_loop.start()
+
         """
         Solver 1 (Error): https://stackoverflow.com/questions/46710299/why-does-pyqt-crashes-without-information-exit-code-0xc0000409
         # PyQT线程安全的，在这里启动多线程，发现连接没有开
@@ -111,19 +126,26 @@ class Track2D(QWidget):
         )
         self._mutex.unlock()
         self.writer.start()
+        ========================================================
+        # Sovler 2:
+        # https://github.com/nsqio/pynsq/issues/128
+        response2 = requests.get(url=self.write_addr, params=transfer_data)
+        print("send finish")
+        self.finish_pub(response2)
+        
         """
-        # # Sovler 2:
-        # # https://github.com/nsqio/pynsq/issues/128
-        # response2 = requests.get(url=self.write_addr, params=transfer_data)
-        # print("send finish")
-        # self.finish_pub(response2)
+
 
     def finish_pub(self, con, data):
         print(data)
-        # if data.decode('utf8') == "OK":
-        #     print("发送请求成功")
-        # else:
-        #     print("发送请求失败，请检查nsq服务后重试")
+        # 退出程序
+        self.io_loop.stop()
+        try:
+            assert data.decode('utf8') == "OK", "发送请求失败，请检查nsq服务后重试"
+            print("发送请求成功")
+            # 这里调用对应的函数
+        except Exception  as e:
+            print(e)
 
     def analysis_video(self):
         # todo 这里要把上传视频和分析视频分开，分开后通过组件时间获取各个按钮的状态,统一在这里传送给后端
@@ -139,93 +161,29 @@ class Track2D(QWidget):
                 'tracking': 'Sort',
             },
         }
-        self.pub_message(data, topic_name='Upload_Vid_Channel')
+
 
 
     def export_metrics(self):
         button_name = self.sender().text()
         print("点击的按钮名称：", button_name)
-        handler = partial(self.recv_message)
-        nsq.Reader(
-            message_handler = handler,
-            nsqd_tcp_addresses = ['127.0.0.1:4150'],
-            topic = 'results',
-            channel = 'GroupA')
-        # file_dialog = QFileDialog()  # 用于显示文件对话框
-        # # 显示保存文件的对话框，并返回用户选择的文件路径和名称
-        # metrics_path, _ = file_dialog.getSaveFileName(self, '导出数据指标文件', '', 'CSV Files (*.csv)')
-        # csv_file_path = os.path.join(metrics_path, f"{video_name_without_extension}.csv")
-        # file_dir = r'E:\data\3D_pre\D1_T1\sortTracker\1_6PPD1ppm'
-        # file_name = video_name_without_extension + '.csv'
-        # # file_name = '2021_10_11_21_49_59_ch03.csv'
-        # # csv_file = r'E:/data/3D_pre/D1_T1/sortTracker/1_6PPD1ppm/2021_10_11_21_49_59_ch03.csv'
-        # csv_file = os.path.join(file_dir, file_name)
-        #
-        # shutil.copyfile(csv_file, metrics_path)
-        # print('CSV 文件导出完成:', metrics_path)
+
 
     def save_video(self):
         # 视频保存的功能实现
         button_name = self.sender().text()
         print("点击的按钮名称：", button_name)
-        handler = partial(self.recv_message)
-        nsq.Reader(
-            message_handler = handler,
-            nsqd_tcp_addresses = ['127.0.0.1:4150'],
-            topic = 'results',
-            channel = 'GroupA')
+        # 同upload函数，发送一个请求，等待返回
+        # handler = partial(self.recv_message)
+        # nsq.Reader(
+        #     message_handler = handler,
+        #     nsqd_tcp_addresses = ['127.0.0.1:4150'],
+        #     topic = 'results',
+        #     channel = 'GroupA')
 
-        # file_dialog = QFileDialog()
-        # save_path, _ = file_dialog.getSaveFileName(self, '保存视频', '', 'Video Files (*.mp4)')
-        # nsq_url = '127.0.0.1:4150'  # NSQ的URL
-
-        # writer = nsq.Writer([nsq_url])
-        # tornado.ioloop.PeriodicCallback(
-        #     functools.partial(self.pub_message, writer),
-        #     # pub_message,
-        #     1000).start()
-        # nsq.run()
 
         # 在此处添加视频保存的代码逻辑
 
-
-# class NSQWriter(QThread):
-#     # 自定义信号对象。参数str就代表这个信号可以传一个字符串
-#     trigger = pyqtSignal(str)
-#     write_info = nsq.Writer(['127.0.0.1:4150'])
-#
-#     def __int__(self):
-#         # 初始化函数
-#         super(NSQWriter, self).__init__()
-#           # todo 每次点击只上传一次内容，不是循环发送
-#
-#     def finish_pub(self, conn, data):
-#         if data.decode('utf8') == "OK":
-#             print("发送请求成功")
-#         else:
-#             print("发送请求失败，请检查nsq服务后重试")
-#
-#     def run(self):
-#         #重写线程执行的run函数
-#         #触发自定义信号
-#         data = {
-#             'video_name': "video_name",
-#             'export_tracking_index': True,
-#             'export_behavior_index': True,
-#             'export_tracking_video': True,
-#             'algorithm': {
-#                 'OneStage': False,
-#                 'detecting': 'YOLO5',
-#                 'tracking': 'Sort',
-#             }
-#         }
-#         transfer_data = json.dumps(data).encode('utf8')
-#
-#         self.write_info.pub(
-#             'Upload_Vid_Channel',
-#             transfer_data,
-#             self.finish_pub
-#         )
 
 def nsq_start():
     pid = os.getpid()  # 获取当前进程的进程ID
@@ -251,11 +209,8 @@ if __name__ == '__main__':
     for p in processes:
         p.join()
 
+    # https://www.jianshu.com/p/c8197304cd6b
     # https://stackoverflow.com/questions/64522179/after-nsq-run-my-python-script-is-not-executing-block-of-code-in-pynsq-packa
     # nsq.run() function starts an event loop in the background.
     nsq.run()
-    # print("exit")
-    # app = QApplication(sys.argv)
-    # fish = Track2D()
-    # fish.show()
-    # sys.exit(app.exec_())
+
